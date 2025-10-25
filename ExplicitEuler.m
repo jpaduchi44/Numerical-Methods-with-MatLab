ab5853bd87db63120bac5dd7862653784ccb74c4
jpaduchi44 <j.paduchi44@gmail.com>
2025-10-26 02:13:16 +0300

    Delete ExplicitEuler.m

--- ExplicitEuler.m
+++
@@ -1,9 +0,0 @@
-function [t, y] = ExplicitEuler(a, b, y0, N, f)
-  t = linspace(a, b, N+1)';
-  h = (b - a) / N;
-  y = zeros(N+1, 1);
-  y(1) = y0;
-  for i = 1:N
-    y(i+1) = y(i) + h * f(t(i), y(i));
-  endfor
-end
